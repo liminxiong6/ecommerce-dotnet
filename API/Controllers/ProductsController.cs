6697021c753bcd8750efa0c8fd5e187dbbdba999
BBeerBear <xiongx659@gmail.com>
2024-10-07 22:38:27 -0400

    Add sorting, filtering, pagination

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using System;
+using API.RequestHelpers;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
@@ -15,11 +16,14 @@ public class ProductsController(IGenericRepository<Product> repo) : ControllerBa
 {
 
     [HttpGet]
-    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type, string? sort)
+    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts([FromQuery]ProductSpecParams specParams)
     {
-        var spec = new ProductSpecification(brand, type, sort);
+        var spec = new ProductSpecification(specParams);
         var products = await repo.ListAsync(spec);
-        return Ok(products);
+        var count = await repo.CountAsync(spec);
+
+        var pagination = new Pagination<Product>(specParams.PageIndex, specParams.PageSize, count, products);
+        return Ok(pagination);
     }
 
     [HttpGet("{id:int}")]
